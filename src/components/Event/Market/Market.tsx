efedf5a6968e82c010512ce4996e93853ca63314
Angelo Leva <angelokta7@gmail.com>
2022-12-23 15:14:22 -0500

    feat(Market & Bet): add findBet function

--- src/components/Event/Market/Market.tsx
+++ src/components/Event/Market/Market.tsx
@@ -3,15 +3,15 @@ import { AppStore } from '@/redux/store';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Selection } from '../Selection';
+import { findBet } from '@/utilities/findBet.utility';
 import './styles/Market.scss';
 
 const Market : React.FC<IMarket> = ({id, name, selections}) => {
 	const stateBets = useSelector((store: AppStore) => store.bets);
-	const findBet = (selectionId: string) => !!stateBets.find(b => b.id === selectionId);
 
 	return (
 		<div className='market'>
-			<div className="market__name">🏆 {name}</div>
+			<div className="market__name">{`🏆 ${name}`}</div>
 			<div className="market__selections">
 				{selections.map(selection => (
 					<Selection
@@ -19,7 +19,7 @@ const Market : React.FC<IMarket> = ({id, name, selections}) => {
 						name={selection.name}
 						price={selection.price}
 						id={selection.id}
-						isSelected={findBet(selection.id)} />
+						isSelected={findBet(selection.id, stateBets)} />
 				))}
 			</div>
 		</div>
--- src/components/Event/Selection/Selection.tsx
+++ src/components/Event/Selection/Selection.tsx
@@ -5,17 +5,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addBet, removeBet } from '@/redux/states/bets';
 import { AppStore } from '@/redux/store';
 import { isOpen } from '@/redux/states/slidebets';
-
+import { findBet } from '@/utilities/findBet.utility';
 
 const Selection : React.FC<ISelection> = ({id, name, price, isSelected}) => {
 
 	const dispatch = useDispatch();
 	const stateBets = useSelector((store: AppStore) => store.bets);
-	const findBet = (bet: IBet) => !!stateBets.find(b => b.id === bet.id);
-	const filterBet = (bet: IBet) => stateBets.filter(b => b.id !== bet.id);
 
 	const handleChange = (bet: IBet) => {
-		if(findBet(bet)) {
+		if(findBet(bet.id, stateBets)) {
 			dispatch(removeBet(bet))
 		} else {
 			dispatch(addBet(bet))
@@ -25,7 +23,9 @@ const Selection : React.FC<ISelection> = ({id, name, price, isSelected}) => {
 
 	return (
 		<div className="selection">
-			<button className={isSelected ? 'selection__button is--selected' : 'selection__button'} onClick={()=>handleChange({id, name, price})}>{name} <br /> {price}</button>
+			<button data-testid={id} id={id} className={isSelected ? 'selection__button is--selected' : 'selection__button'} onClick={()=>handleChange({id, name, price})}>
+				{name} <br /> {price}
+			</button>
 		</div>
 	);
 };
